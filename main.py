7070a6c4b5bc749f4cce1ede32ffef9eb3a13525
Osipov Sasha <sashaosipov753@gmail.com>
2020-02-19 17:27:49 +0200

    initial

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ from flask import Flask, escape, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/first')
 def hello():
-    name = request.args.get("name", "World")
-    return f'Osipov Oleksander TI-81'
+    a = None
+    b = ' 86 '
+    c = 12
+    
+    return f"{a},{b},{c}"
 
 if __name__ == '__main__':
     app.run('0.0.0.0')
